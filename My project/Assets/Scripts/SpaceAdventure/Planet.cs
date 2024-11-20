7ac6d2579d2eeab03f5aa2728169fb8b4270160f
SEMENLISENKO <fortsmain911@gmail.com>
2024-11-20 16:42:38 +1000

    First Commit

---
+++ My project/Assets/Scripts/SpaceAdventure/Planet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField]public string planetScene = "";
+    [HideInInspector]public Animator Animator;
+    [HideInInspector] public Button Button;
+    public void Start()
+    {
+        Button = GetComponent<Button>();
+        Animator = GetComponent<Animator>();
+        SpaceMenu.DeselectOther += Deselect;
+    }
+    public void Select()
+    {
+        SpaceMenu.DeselectOther -= Deselect;
+        SpaceMenu.DeselectOther.Invoke();
+        SpaceMenu.SelectedPlanetScene = planetScene;
+        Animator.SetBool("IsSelected", true);
+        SpaceMenu.DeselectOther += Deselect;
+    }
+    public void Deselect()
+    {
+        Animator.SetBool("IsSelected", false);
+    }
+    private void OnDisable()
+    {
+        Button.interactable = false;
+        Button.interactable = true;
+    }
+}
---
+++ My project/Assets/Scripts/SpaceAdventure/SpaceMenu.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceMenu : MonoBehaviour
+{
+    public static Action DeselectOther;
+    public static string SelectedPlanetScene = "";
+
+    [SerializeField] public GameObject Canvas;
+    [SerializeField] public GameObject AdventureLoadScreen;
+    [SerializeField] public float AdventureLoadScreenTime;
+    public bool inGame = true;
+    public void Clear()
+    {
+        SelectedPlanetScene = "";
+    }
+    public void Start()
+    {
+        DeselectOther += Clear;
+        Canvas.SetActive(false);
+        DontDestroyOnLoad(this);
+        StartCoroutine(MenuExecutor());
+    }
+    public IEnumerator MenuExecutor()
+    {
+        yield return new WaitForSeconds(0.1f);
+        yield return new WaitUntil(() => Input.GetButtonDown("MAP") && inGame);
+
+        Canvas.SetActive(true);
+
+        yield return new WaitForSeconds(0.1f);
+        yield return new WaitUntil(() => Input.GetButtonDown("MAP") || !inGame);
+
+        Canvas.SetActive(false);
+        StartCoroutine(MenuExecutor());
+    }
+    public void InvokeCoroutineCP()
+    {
+        StartCoroutine(ChangePlanet());
+    }
+    public IEnumerator ChangePlanet()
+    {
+        if (Equals(SelectedPlanetScene, ""))
+        {
+            yield break;
+        }
+        AdventureLoadScreen.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        AdventureLoadScreen.SetActive(false);
+        SceneManager.LoadScene(SelectedPlanetScene);
+        Canvas.SetActive(false);
+        yield break;
+    }
+}
